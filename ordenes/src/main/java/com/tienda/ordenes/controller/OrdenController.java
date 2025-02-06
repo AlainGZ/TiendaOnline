68708cd7bcb7c9ae57c63d40fe3fd909658b3a18
AlainGZ <devalgz955@gmail.com>
2025-02-06 14:07:14 -0500

    Se crea la clase OrdenController

---
+++ ordenes/src/main/java/com/tienda/ordenes/controller/OrdenController.java
@@ -0,0 +1,38 @@
+package com.tienda.ordenes.controller;
+
+import com.tienda.ordenes.model.Orden;
+import com.tienda.ordenes.service.OrdenService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ordenes")
+public class OrdenController {
+	private final OrdenService ordenService;
+
+	public OrdenConroller(OrdenService ordenService){
+		this.ordenService = ordenService;
+	}
+
+	@GetMapping
+	public List<Orden> listarOrdenes(){
+		return ordenService.obtenerTodas();
+	}
+
+	@GetMapping("/usuario/{usuarioId}")
+	public List<Orden> listarOrdenesPorUsuario(@PathVariable Long usuarioId){
+		return ordenService.obtenerPorUsuario(usuarioId);
+	}
+
+	@PostMapping
+	public Orden crearOrden(@RequestBody Orden orden){
+		return ordenService.crearOrden(orden);
+	}
+
+	@DeleteMapping("/{id}")
+	public String eliminarOrden(@PathVariable Long id){
+		ordenService.eliminarOrden(id);
+		return "Orden eliminada con exito";
+	}
+}
