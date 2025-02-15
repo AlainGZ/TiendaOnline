10e89b97a44e6dafbc60cb5c50d8fea9ce190727
AlainGZ <devalgz955@gmail.com>
2025-02-15 14:25:03 -0500

    Se atualiza el Service

--- envios/src/main/java/com/tienda/envios/service/EnvioService.java
+++ envios/src/main/java/com/tienda/envios/service/EnvioService.java
@@ -1,7 +1,10 @@
 package com.tienda.envios.service;
 
+import com.tienda.envios.model.Envio;
 import com.tienda.envios.repository.EnvioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnvioService {
@@ -10,7 +13,9 @@ public class EnvioService {
 	public EnvioService(EnvioRepository envioRepository){
 		this.envioRepository = envioRepository;
 	}
-	
+	public List<Envio> obtenerTodos() {
+		return envioRepository.findAll();
+	}
 
 
 }
