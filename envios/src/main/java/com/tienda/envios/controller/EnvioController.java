406b6211fee2260a98f68ece585a9d4569ee5bd4
AlainGZ <devalgz955@gmail.com>
2025-02-18 22:47:03 -0500

    Se actualiza el controlador

--- envios/src/main/java/com/tienda/envios/controller/EnvioController.java
+++ envios/src/main/java/com/tienda/envios/controller/EnvioController.java
@@ -1,4 +1,17 @@
 package com.tienda.envios.controller;
 
+import com.tienda.envios.service.EnvioService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/envios")
 public class EnvioController {
+	private final EnvioService envioService;
+	
+	public EnvioController(EnvioService envioService){
+		this.envioService = envioService;
+	}
+
+
 }
