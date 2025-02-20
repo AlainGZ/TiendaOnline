dd86a558fc6c6dde66f5d07722f5cfe1c98a0294
AlainGZ <devalgz955@gmail.com>
2025-02-19 21:05:06 -0500

    Se crea el archivo JS

---
+++ envios/src/main/resources/static/envios.js
@@ -0,0 +1,52 @@
+document.getElementById('formEnvio').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const ordenId = parseInt(document.getElementById('ordenId').value);
+    const estado = document.getElementById('estado').value;
+    const trackingNumber = document.getElementById('trackingNumber').value;
+    const mensajeEnvio = document.getElementById('mensajeEnvio');
+
+    try {
+        const response = await fetch('http://localhost:8080/envios', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body:JSON.stringify({ordenId, estado, trackingNumber}) 
+        });
+
+        if(response.ok){
+            mensajeEnvio.textContent = 'Envio creado con exito';
+            mensajeEnvio.style.color = 'green';
+        }else{
+            mensajeEnvio.textContent = 'Error al crear el envio';
+            mensajeEnvio.style.color = 'red';
+        }
+        cargarEnvios();
+        
+    } catch (error) {
+        mensajeEnvio.textContent = 'Error al conectar con el servidor.';
+        mensajeEnvios.style.color = 'red';
+    }
+
+});
+
+async function cargarEnvios(){
+    const listaEnvios = document.getElementById('listarEnvios');
+    listaEnvios.innerHTML = '';
+
+    try {
+        const response = await fetch('http://localhost:8080/envios');
+        const envios = await response.json();
+
+        envios.forEach(envios => {
+            const li = document.createElement('li');
+            li.textContent = `ID: ${envio.id} | Orden: ${envio.ordenId} | Estado: ${envio.estado} | Tracking: ${envio.trackingNumber} | Fecha: ${envio.fechaEnvio}`;
+            listaEnvios.appendChild(li);
+            
+        });
+
+    } catch (error) {
+        listaEnvios.innerHTML = '<li>Error al cargar los envios</li>';
+    }
+} 
