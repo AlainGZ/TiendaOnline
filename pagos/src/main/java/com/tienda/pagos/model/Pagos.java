da001a941f4dfaf560b1cfe52213d362612c56f4
AlainGZ <devalgz955@gmail.com>
2025-02-23 16:14:25 -0500

    Se actualiza la clase pagos en el model

--- pagos/src/main/java/com/tienda/pagos/model/Pagos.java
+++ pagos/src/main/java/com/tienda/pagos/model/Pagos.java
@@ -1,11 +1,15 @@
 package com.tienda.pagos.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="pagos")
@@ -14,4 +18,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class Pagos {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private Long ordenId;
+
+	private double monto;
+
+	private String metodo;
+
+	private String estado;
+
+	private LocalDateTime fechaPago;
+
+	private Long referencia;
 }
