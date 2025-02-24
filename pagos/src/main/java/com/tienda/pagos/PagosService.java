18ae9bef7fd267f6d56860b815e2c965fbe1a070
AlainGZ <devalgz955@gmail.com>
2025-02-24 14:27:08 -0500

    Se crean metodos Read en el Service

--- pagos/src/main/java/com/tienda/pagos/PagosService.java
+++ pagos/src/main/java/com/tienda/pagos/PagosService.java
@@ -1,13 +1,36 @@
 package com.tienda.pagos;
 
+import com.tienda.pagos.model.Pagos;
 import com.tienda.pagos.repository.PagosRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class PagosService {
 	private final PagosRepository pagosRepository;
 
-	private PagosService(PagosRepository pagosRepository){
+	public PagosService(PagosRepository pagosRepository){
 		this.pagosRepository = pagosRepository;
 	}
+
+	//metodo para traer_todo
+	public List<Pagos> obtenerTodos(){
+		return pagosRepository.findAll();
+	}
+
+	//metodo para buscar por el id
+	public Optional<Pagos> obtenerPorId(Long id){
+		return pagosRepository.findById(id);
+	}
+
+	//metodo para buscar por el ordenId
+
+	//metodo  para crear un pago
+
+	//metodo para actualizar un pago
+
+	//metodo para eliminar un pago
 }
