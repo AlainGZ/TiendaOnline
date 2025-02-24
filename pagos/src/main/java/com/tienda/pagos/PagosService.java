67af533a5f9edebe616c5d872b89fe8161a4a35e
AlainGZ <devalgz955@gmail.com>
2025-02-24 11:14:04 -0500

    Se agrega un objeto del tipo repository y se agrega su constructor

--- pagos/src/main/java/com/tienda/pagos/PagosService.java
+++ pagos/src/main/java/com/tienda/pagos/PagosService.java
@@ -1,7 +1,13 @@
 package com.tienda.pagos;
 
+import com.tienda.pagos.repository.PagosRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PagosService {
+	private final PagosRepository pagosRepository;
+
+	private PagosService(PagosRepository pagosRepository){
+		this.pagosRepository = pagosRepository;
+	}
 }
