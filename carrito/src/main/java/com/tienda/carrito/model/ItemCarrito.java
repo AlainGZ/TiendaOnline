61141222730e7ac617e8cd8d632882a8f4c68250
AlainGZ <devalgz955@gmail.com>
2025-03-06 19:22:01 -0500

    Se actualiza el ItemCarrito

--- carrito/src/main/java/com/tienda/carrito/model/ItemCarrito.java
+++ carrito/src/main/java/com/tienda/carrito/model/ItemCarrito.java
@@ -1,4 +1,22 @@
 package com.tienda.carrito.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "item_carrito")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class ItemCarrito {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long producto;
+	private int cantidad;
 }
