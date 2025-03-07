4046621cd04635ebff3f967415d1ce824f4e7352
AlainGZ <devalgz955@gmail.com>
2025-03-07 18:50:33 -0500

    Se instancia el repositorio y se crea el constructor

--- carrito/src/main/java/com/tienda/carrito/service/CarritoService.java
+++ carrito/src/main/java/com/tienda/carrito/service/CarritoService.java
@@ -1,4 +1,16 @@
 package com.tienda.carrito.service;
 
+import com.tienda.carrito.repository.CarritoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarritoService {
+	private final CarritoRepository carritoRepository;
+
+	public CarritoService(CarritoRepository carritoRepository){
+		this.carritoRepository = carritoRepository;
+	}
+
+	
+
 }
