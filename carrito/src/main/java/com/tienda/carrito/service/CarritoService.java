7917a8e7285aff2f3af9d4acd9a24fad1903431b
AlainGZ <devalgz955@gmail.com>
2025-03-08 15:53:16 -0500

    Se crea el metodo obtener o crear la entidad

--- carrito/src/main/java/com/tienda/carrito/service/CarritoService.java
+++ carrito/src/main/java/com/tienda/carrito/service/CarritoService.java
@@ -1,7 +1,10 @@
 package com.tienda.carrito.service;
 
+import com.tienda.carrito.model.Carrito;
 import com.tienda.carrito.repository.CarritoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class CarritoService {
@@ -11,6 +14,9 @@ public class CarritoService {
 		this.carritoRepository = carritoRepository;
 	}
 
-
+	public Carrito ontenerOCrearCarrito(Long usuarioId){
+		return carritoRepository.findByUsuarioId(usuarioId)
+				.orElseGet(() -> carritoRepository.save(new Carrito(null, usuarioId, new ArrayList<>())));
+	}
 
 }
