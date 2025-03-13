976181eb34cdc258a80fdf88c4719e35922ba865
AlainGZ <devalgz955@gmail.com>
2025-03-12 19:17:53 -0500

    Se crea el Get obtener carrito

--- carrito/src/main/java/com/tienda/carrito/controller/CarritoController.java
+++ carrito/src/main/java/com/tienda/carrito/controller/CarritoController.java
@@ -1,9 +1,23 @@
 package com.tienda.carrito.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tienda.carrito.model.Carrito;
+import com.tienda.carrito.service.CarritoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/carrito")
 public class CarritoController {
+	private final CarritoService carritoService;
+
+	public CarritoController (CarritoService carritoService){
+		this.carritoService = carritoService;
+	}
+
+	@GetMapping("/{usuarioId}")
+	public Carrito obtenerCarrito(@PathVariable Long usuarioId){
+		return carritoService.obntenerOCrearCarrito(usuarioId);
+	}
+
 }
